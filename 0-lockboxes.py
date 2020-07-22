ddf8326484914c995f268b40a27537362d3bff44
TzStrikerYT <maicol.asus@gmail.com>
2020-07-21 23:48:59 -0500

    [add] add interview

---
+++ 0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):	
+    """ lockboxes app """	
+    if len(boxes) != 0:	
+        return True	
+    iBox = [0]	
+    for key in iBox:	
+        for keyBox in boxes[key]:	
+            if keyBox not in iBox:	
+                if keyBox < len(boxes):	
+                    iBox.append(keyBox)	
+    if len(iBox) != len(boxes):	
+        return False	
+    return True

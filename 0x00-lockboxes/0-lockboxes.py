0332d527e57c8dcb78195bd3058d8f224341628c
TzStrikerYT <maicol.asus@gmail.com>
2020-07-22 00:06:23 -0500

    [heeeelp]

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,14 +3,14 @@
 
 def canUnlockAll(boxes):	
     """ lockboxes app """	
-    if len(boxes) != 0:	
-        return True	
+    if len(boxes) == 0:	
+        return False	
     iBox = [0]	
     for key in iBox:	
         for keyBox in boxes[key]:	
             if keyBox not in iBox:	
                 if keyBox < len(boxes):	
                     iBox.append(keyBox)	
-    if len(iBox) != len(boxes):	
-        return False	
-    return True
+    if len(iBox) == len(boxes):	
+        return True	
+    return False

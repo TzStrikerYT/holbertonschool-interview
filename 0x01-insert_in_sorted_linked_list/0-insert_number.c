f51757c45e174231fe93a72ccb81f618a688c42b
TzStrikerYT <maicol.asus@gmail.com>
2020-07-23 21:54:42 -0500

    [add] add c insertion interview

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * function that inserts a new node in a sorted linked list
+ * @head: head of the list
+ * @number: number tha shod be inserted
+ * Return: node of list, or NULL if op fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *actual, *new_node;
+    
+    new_node = malloc(sizeof(listint_t));
+    
+    if(!new_node)
+        return(NULL);
+    
+    new_node->n = number;
+    
+    if(!head)
+    {
+        new_node->next = NULL;
+        *head = new_node;
+        return (new_node);
+    }
+    
+    actual = *head;
+    
+    while (actual)
+    {
+        if (actual->n <= number && actual->next->n >= number)
+        {
+            new_node->next = actual->next;
+            actual->next = new_node;
+            return (new_node);
+        }
+        actual = actual->next;
+    }
+    return (NULL);
+}

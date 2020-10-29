c7c2a5ac872eec4a3eba24752436f3890de312cd
TzStrikerYT <maicol.asus@gmail.com>
2020-10-28 20:23:09 -0500

    [add] add test

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,11 +2,11 @@
 import sys
 
 
-def nqueens(n):
+def nQueens(n):
     """ The N queens puzzle is the challenge
             of placing N non-attacking queens on an N×N chessboard
     """
-    def nqueensv2(queens, cord_dif, cord_sum):
+    def recursion(queens, cord_dif, cord_sum):
         """ Recursive function
         """
         p = len(queens)
@@ -15,10 +15,10 @@ def nqueens(n):
             return None
         for q in range(n):
             if q not in queens and p-q not in cord_dif and p+q not in cord_sum:
-                nqueensv2(queens + [q], cord_dif + [p - q], cord_sum + [p + q])
+                recursion(queens + [q], cord_dif + [p - q], cord_sum + [p + q])
     result = []
     final_result = []
-    nqueensv2([], [], [])
+    recursion([], [], [])
     for row in result:
         for i, col in enumerate(row):
             coord = [i, col]
@@ -26,17 +26,16 @@ def nqueens(n):
         print(final_result)
         final_result = []
 
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         exit(1)
     try:
         n = int(sys.argv[1])
-    except ValueError:
+    except:
         print("N must be a number")
         exit(1)
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
-    nqueens(n)
+    nQueens(n)
